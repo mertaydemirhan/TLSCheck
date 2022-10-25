9b24e18aaff6470e2008ba55092fe9561c8f49de
Mertay Demirhan <mertay.demirhan@gmail.com>
2022-10-25 17:17:44 +0300

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblAlarm = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblAlarm
+            // 
+            this.lblAlarm.AutoSize = true;
+            this.lblAlarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlarm.Location = new System.Drawing.Point(95, 88);
+            this.lblAlarm.Name = "lblAlarm";
+            this.lblAlarm.Size = new System.Drawing.Size(211, 29);
+            this.lblAlarm.TabIndex = 0;
+            this.lblAlarm.Text = "Priority AÇILDI....";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 263);
+            this.Controls.Add(this.lblAlarm);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TLS Check Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblAlarm;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public readonly string ServerAdress = ConfigurationManager.AppSettings["ServerAdress"];
+        public readonly string DatabaseName = ConfigurationManager.AppSettings["DatabaseName"];
+        public readonly string UsrName = ConfigurationManager.AppSettings["UsrName"];
+        public readonly string Pw = ConfigurationManager.AppSettings["Pw"];
+        SqlConnection baglanti;
+        SqlDataAdapter sqlAdapter;
+        SqlCommand komut;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblAlarm.Visible = false;
+            timer1.Start();
+        }
+
+        WMPLib.WindowsMediaPlayer Player;
+
+        private void PlayFile(String url)
+        {
+            Player = new WMPLib.WindowsMediaPlayer();
+            Player.PlayStateChange +=
+                new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(Player_PlayStateChange);
+            Player.MediaError +=
+                new WMPLib._WMPOCXEvents_MediaErrorEventHandler(Player_MediaError);
+            Player.URL = url;
+            Player.settings.autoStart = true;
+            Player.settings.setMode("loop", true);
+            Player.controls.play(); 
+        }
+
+        private void Player_PlayStateChange(int NewState)
+        {
+            if ((WMPLib.WMPPlayState)NewState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                this.Close();
+            }
+        }
+
+        private void Player_MediaError(object pMediaObject)
+        {
+            MessageBox.Show("Cannot play media file.");
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var url = "https://pos.tlscontact.com/ist_en/eshop/services/";
+
+            var wc = new WebClient();
+            var arr = wc.DownloadData(url);
+            var txt = Encoding.UTF8.GetString(arr);
+
+            var ret = txt.Contains("£250.00");
+
+            if (ret)
+            {
+                lblAlarm.Visible = true;
+                baglanti = new SqlConnection("Server=" + ServerAdress + ";Database=" + DatabaseName + ";User Id=" + UsrName + ";Password=" + Pw + ";");
+                baglanti.Open();
+                komut = new SqlCommand($"USE [StudentApp] GO DECLARE	@return_value int EXEC	@return_value = [dbo].[AutoMailTLS] SELECT	'Return Value' = @return_value GO", baglanti);
+                komut.ExecuteScalar();
+                baglanti.Close();
+                PlayFile(@"Alarm01.wav");
+            }
+        }
+    }
+}
